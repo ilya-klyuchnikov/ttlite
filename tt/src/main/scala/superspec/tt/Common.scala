cf860780df5c20716513c3761dc3f6e3c6d36ad3
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2013-08-16 16:56:16 +0400

    towards metaparser

--- tt/src/main/scala/superspec/tt/Common.scala
+++ tt/src/main/scala/superspec/tt/Common.scala
@@ -2,15 +2,16 @@ package superspec.tt
 
 import org.kiama.output.PrettyPrinter
 
+// TODO: make an object or package object
 trait Common extends PrettyPrinter {
   // meta-syntax
-  trait Name
+  sealed trait Name
   case class Global(n: String) extends Name
   case class Assumed(n: String) extends Name
   case class Local(i: Int) extends Name
   case class Quote(i: Int) extends Name
 
-  trait MetaTerm
+  sealed trait MetaTerm
   case class MApp(t1: MetaTerm, t2: MetaTerm) extends MetaTerm
   case class MAnn(t1: MetaTerm, t2: MetaTerm) extends MetaTerm
   // typed binders
@@ -35,8 +36,30 @@ trait Common extends PrettyPrinter {
 
   case class TypeError(msg: String) extends Exception(msg)
 
-}
+  import scala.language.postfixOps
+  import scala.util.parsing.combinator.{PackratParsers, ImplicitConversions}
+  import scala.util.parsing.combinator.syntactical.StandardTokenParsers
+  import scala.util.parsing.combinator.lexical.StdLexical
 
+  class MetaLexical extends StdLexical {
+    import scala.util.parsing.input.CharArrayReader._
+    override def whitespace: Parser[Any] = rep(
+      whitespaceChar
+        | '/' ~ '*' ~ comment
+        | '/' ~ '/' ~ rep( chrExcept(EofCh, '\n') )
+        | '-' ~ '-' ~ rep( chrExcept(EofCh, '\n') )
+        | '/' ~ '*' ~ failure("unclosed comment")
+    )
+    override def identChar = letter | elem('_') | elem('$')
+  }
+
+  object MetaParser extends StandardTokenParsers with PackratParsers with ImplicitConversions {
+    type C = List[String]
+    type Res[A] = C => A
+
+    lazy val term: PackratParser[Res[MetaTerm]] = null
+  }
+}
 
 
 trait REPL extends Common {
