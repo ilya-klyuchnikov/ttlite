3a29e19dd7e1f06fdb6aa4c5778cd40e77e8e0be
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2013-09-29 00:07:50 +0400

    lte_tran proof (a lot of boilerplate)

--- stl/nat.hs
+++ stl/nat.hs
@@ -1,3 +1,5 @@
+import "examples/fin.hs";
+
 plus : forall (x : Nat) (y : Nat). Nat;
 plus =
     \ (x : Nat) (y : Nat) ->
@@ -43,7 +45,7 @@ lemma1 =
                         x)
             y;
 
-
+-- used in lemma3;
 lemma2 : forall (x : Nat) (_ : lte_prop (Succ x) Zero) . lte_prop (Succ x) (Succ Zero);
 lemma2 =
     \ (x : Nat) ->
@@ -52,8 +54,7 @@ lemma2 =
             (\ (_ : lte_prop (Succ Zero) Zero) -> Triv)
             (\ (x : Nat)
                (rec : forall (_ : lte_prop (Succ x) Zero) . lte_prop (Succ x) (Succ Zero))
-               (k : lte_prop (Succ (Succ x)) Zero) ->
-                    k)
+               (bot : lte_prop (Succ (Succ x)) Zero) -> bot)
             x;
 
 
@@ -68,9 +69,8 @@ lemma3 =
                     (\ (_ : lte_prop Zero Zero) -> Triv)
                     ( \ (x : Nat)
                         (rec : forall (_ : lte_prop x Zero) . lte_prop x (Succ Zero))
-                        (p : lte_prop (Succ x) Zero) ->
+                        (p : lte_prop (Succ x) Zero) -> lemma2 x p)
                             -- (?? : lte_prop (Succ x) (Succ Zero))
-                            lemma2 x p)
                     x)
             ( \ (y : Nat)
                 (rec : forall (x : Nat) (_ : lte_prop x y) . lte_prop x (Succ y))
@@ -86,21 +86,51 @@ lemma3 =
                         x)
             y;
 
---    y <= z     x <= y    x <= z
--- ---------------------------------
---          x <=
-$lemma4 :
-    forall (z : Nat) (rec : forall (y : Nat) (_ : lte_prop y z) (x : Nat) (_ : lte_prop x y) . lte_prop x z)
-           (y : Nat) (_ : lte_prop y (Succ z))
-           (x : Nat) (_ : lte_prop x y) .
-                lte_prop x (Succ z);
+lemma4 :
+    forall (z : Nat) (x : Nat) (l2 : lte_prop x Zero) . lte_prop x (Succ z);
+lemma4 =
+    \ (z : Nat) (x : Nat) ->
+        elim Nat
+            (\ (x : Nat) -> forall (l2 : lte_prop x Zero) . lte_prop x (Succ z) )
+            (\ (l2 : lte_prop Zero Zero) . Triv)
+            (\ (x : Nat)
+               (rec : forall (l2 : lte_prop x Zero) . lte_prop x (Succ z))
+               (l2 : lte_prop (Succ x) Zero) -> abort (lte_prop (Succ x) (Succ z)) l2)
+            x;
 
-{-
-$theorem :
+lte_tran :
     forall (z : Nat)
            (y : Nat) (_ : lte_prop y z)
            (x : Nat) (_ : lte_prop x y) . lte_prop x z;
-
-$theorem :
-    forall (x : Nat) (y : Nat) (z : Nat) (lte1 : lte_prop x y) (lte1 : lte_prop y z) . lte_prop x z;
--}
+lte_tran =
+    \ (z : Nat) ->
+        elim Nat
+            (\ (z : Nat) -> forall (y : Nat) (_ : lte_prop y z) (x : Nat) (_ : lte_prop x y) . lte_prop x z)
+            -- z = Zero
+            lemma1
+            -- z = S z1
+            (\ (z1 : Nat)
+               (recZ : forall (y : Nat) (_ : lte_prop y z1) (x : Nat) (_ : lte_prop x y) . lte_prop x z1)
+               (y : Nat) ->
+                    elim Nat
+                        (\ (y : Nat) -> forall (_ : lte_prop y (Succ z1)) (x : Nat) (_ : lte_prop x y) . lte_prop x (Succ z1))
+                        -- y = Z
+                        (\ (l1 : lte_prop Zero (Succ z1)) -> lemma4 z1)
+                        -- y = S y1
+                        (\ (y1 : Nat)
+                           (recY : forall (_ : lte_prop y1 (Succ z1)) (x : Nat) (_ : lte_prop x y1) . lte_prop x (Succ z1))
+                           (lte_y1_z1 : lte_prop (Succ y1) (Succ z1))
+                           (x : Nat) ->
+                                elim Nat
+                                    (\ (x : Nat) -> forall (_ : lte_prop x (Succ y1)) . lte_prop x (Succ z1) )
+                                    -- x == Z
+                                    (\ (_ : lte_prop Zero (Succ y1)) -> Triv )
+                                    -- x == S x1
+                                    ( \ (x1 : Nat)
+                                        (_ : forall (_ : lte_prop x1 (Succ y1)) . lte_prop x1 (Succ z1))
+                                        (lte_x1_y1 : lte_prop (Succ x1) (Succ y1)) -> recZ y1 lte_y1_z1 x1 lte_x1_y1)
+                                    x
+                           )
+                        y
+               )
+            z;
